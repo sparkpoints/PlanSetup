1617f8d188c8b71f3da2f130f494a8361563c01c
peteryang <sparkpoints@gmail.com>
2020-09-02 16:24:30 +0800

    modify

--- bin/plansetup.py
+++ bin/plansetup.py
@@ -1,8 +1,25 @@
 import os
+import argparse
 
+# difine system base dir
+PatientDataHome = "/pinnacle_patient_expansion/NewPatients/"
+SystemScriptHome = "/usr/local/adacnew/PinnacleSiteData/Scripts/"
+ScriptBinHome = SystemScriptHome + 'bin/'
+ScriptTempDir = SystemScriptHome + 'tmp/'
+ScriptLogDir = SystemScriptHome + 'log/'
+
+# def config or template file
+BeamTemplate = SystemScriptHome + 'BeamTemplate.txt'
+IMRTTemplate = SystemScriptHome + 'IMrTTemplate.txt'
 
 if __name__ == "__main__":
-    PatientDataBaseHome = "/pinnacle_patient_expansion/NewPatients/"
-    SystemScriptHome = "/usr/local/adacnew/PinnacleSiteData/Scripts/"
-    BinHome = SystemScriptHome + 'bin/'
-    ScriptTempDir = 
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mrn',help='medical record number')
+    parser.add_argument('path',help='plan data path')
+    args = parser.parse_args()
+    
+    print(args.mrn,args.path)
+    # def temp and log file
+    
+    print(ScriptBinHome,ScriptTempDir)
